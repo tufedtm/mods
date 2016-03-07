0cd27a4a305e66c16e19e370e9885f8907897b13
tufedtm <tufedtm@gmail.com>
2016-03-07 21:21:26 +0300

    +get_data_sections_2000

--- magazines/igromania/includes/getters.py
+++ magazines/igromania/includes/getters.py
@@ -1,5 +1,35 @@
+# coding=utf-8
+from __future__ import unicode_literals
+
 import os
+import configparser
+from settings import MAGAZINE
 
 
 def get_magazines(src_path):
+    """
+    возвращает все журналы за год
+
+    :param src_path: путь к папке с папками 12 журналов за год
+    :return: изменяемый список с названиями папок
+    """
     return os.listdir(src_path)
+
+
+def get_data_sections_2000():
+    """
+    возвращает все секции за месяц
+
+    :return: изменяемый список с названиями секций
+    """
+    config = configparser.ConfigParser(delimiters='=')
+    config.read('%sdata/data.txt' % MAGAZINE)
+
+    sections = []
+
+    for section in config.sections():
+        if config.has_option(section, 'Install'):
+            break
+        sections.append(section)
+
+    return sections
--- magazines/igromania/includes/settings.py
+++ magazines/igromania/includes/settings.py
@@ -1,1 +1,6 @@
-FOLDER_DEST = 'e:/~~/'
+# coding=utf-8
+from __future__ import unicode_literals
+
+FOLDER_DEST = 'e:/~~~~/'
+
+MAGAZINE = 'e:/~~/игромания 2000/игромания 2000.28.01/'
