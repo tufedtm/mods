997316b8667730372e402c5766b93511591cbcd4
tufedtm <tufedtm@gmail.com>
2016-03-08 22:43:14 +0300

    refactor datafill admin

--- datafill/admin.py
+++ datafill/admin.py
@@ -2,30 +2,17 @@ from django.contrib import admin
 from models import Magazine, Game, Patch
 
 
-class MagazineAdmin(admin.ModelAdmin):
-    list_display = ['title_display']
+class ReadOnlyFields(admin.ModelAdmin):
     readonly_fields = ['id', 'created', 'updated']
 
-    fieldsets = (
-        (None, {
-            'fields': ('title', 'number', 'public_year', 'public_month', 'cover', (
-                'id', 'created', 'updated'
-            )),
-        }),
-    )
+
+class MagazineAdmin(ReadOnlyFields):
+    list_display = ['title_display']
 
     def title_display(self, obj):
         return '%s %s' % (obj.get_title_display(), obj.number)
 
 
-class GameAdmin(admin.ModelAdmin):
-    readonly_fields = ['id', 'created', 'updated']
-
-
-class PatchAdmin(admin.ModelAdmin):
-    readonly_fields = ['id', 'created', 'updated']
-
-
 admin.site.register(Magazine, MagazineAdmin)
-admin.site.register(Game, GameAdmin)
-admin.site.register(Patch, PatchAdmin)
+admin.site.register(Game, ReadOnlyFields)
+admin.site.register(Patch, ReadOnlyFields)
