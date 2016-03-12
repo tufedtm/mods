8031cd076fa76cad4c69b4e446589beb087b0500
tufedtm <tufedtm@gmail.com>
2016-03-12 17:59:19 +0300

    + копирование demosthemes1

--- magazines/igromania/igromania.py
+++ magazines/igromania/igromania.py
@@ -4,7 +4,8 @@ import os
 import shutil
 from pprint import pprint
 from includes.getters import get_magazines
-from includes.getters2004 import get_gamezone, get_themesdvd, get_deathzone
+from includes.getters2004 import get_gamezone, get_themesdvd, get_deathzone, get_demosthemes1, get_demosthemes2, \
+    get_interest, get_patches
 from includes.settings import FOLDER_DEST, MAGAZINE_FOLDER
 
 
@@ -80,3 +81,24 @@ def copy_deathzone():
             print(dst)
             shutil.copytree(src, dst)
             break
+
+
+def copy_demosthemes1():
+    data = get_demosthemes1()
+
+    for item in data.values():
+        print(item['Title'])
+        path = '%s/%s' % (item['Path'].split('\\')[0], item['Path'].split('\\')[1])
+
+        src = '%s%s' % (MAGAZINE_FOLDER, path)
+        print(src)
+        dst = '%sdemosthemes1/%s/%s' % (
+            FOLDER_DEST, MAGAZINE_FOLDER.split('/')[3], item['Title'].replace(':', '-').replace('"', '')
+        )
+        print(dst)
+        shutil.copytree(src, dst)
+
+    ini = MAGAZINE_FOLDER + 'DemosThemes1/data.ini'
+    dest = FOLDER_DEST + 'demosthemes1/' + MAGAZINE_FOLDER.split('/')[3]
+
+    shutil.copy(ini, dest)
